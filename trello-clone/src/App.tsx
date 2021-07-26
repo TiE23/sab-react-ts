86d22c082b555707ebdb41c5446726f49b6c1c65
Kyle Geib <kyle.geib@gmail.com>
2021-07-26 15:57:56 -0400

    Importing SVG as a component

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react'; // Don't need to import React anymore. That's cool.
+// import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';  // Using react-scripts to import an SVG as a component.
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Logo title="Some Title" className="App-logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, again!
         </a>
       </header>
     </div>
