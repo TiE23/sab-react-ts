a29ab6c87b5a8fbe437a2854ad15009a1df1847f
Kyle Geib <kyle.geib@gmail.com>
2021-08-04 17:34:09 -0400

    Smarter type management

--- trello-clone/src/withInitialState.tsx
+++ trello-clone/src/withInitialState.tsx
@@ -6,24 +6,31 @@ type InjectedProps = {
   initialState: AppState,
 };
 
+type PropsWithoutInjected<TBaseProps> = Omit<TBaseProps, keyof InjectedProps>;
+
 /**
  * An "Injector HOC".
  * @param WrappedComponent A component that has an intersection of props.
  * @returns
  */
 export function withInitialState<TProps>(
-  WrappedComponent: ComponentType<TProps & InjectedProps>
+  // WrappedComponent: ComponentType<TProps & InjectedProps>
+  WrappedComponent: ComponentType<PropsWithoutInjected<TProps> & InjectedProps>
 ) {
   // Omit is a TS type that removes initialState prop from the given Props.
   // This forcibly removes any user's attempt to add their own "initialState" prop.
-  return (props: Omit<TProps, keyof InjectedProps>) => {
+  // return (props: Omit<TProps, keyof InjectedProps>) => {
+
+  // But we do a slight improvement and adopt "PropsWithoutInjected".
+  return (props: PropsWithoutInjected<TProps>) => {
     const [initialState, setInitialState] = useState < AppState>({
       lists: [],
       draggedItem: null,
     });
     return (
       <WrappedComponent
-        {...props as TProps}  // This is important because TS isn't that smart!
+        // {...props as TProps}  // This is important because TS isn't that smart!
+        {...props}  // Cleaner using PropsWithoutInjected
         initialState={initialState}
       />
     );
