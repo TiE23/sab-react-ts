c48d107fd9266fa178d90addb49de3c648637f6c
Kyle Geib <kyle.geib@gmail.com>
2021-08-09 08:30:20 -0400

    Example of Mocking a component file

--- testing/src/App.spec.tsx
+++ testing/src/App.spec.tsx
@@ -1,17 +1,36 @@
-import React from "react"
-import { App } from "./App"
-import { createMemoryHistory } from 'history'
-import { render } from "@testing-library/react"
-import { Router } from "react-router-dom"
+import React from "react";
+import { App } from "./App";
+import { createMemoryHistory } from 'history';  // This is an extra library.
+import { render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+
+// This mocks Home.tsx and returns <div>Home</div> in its place.
+jest.mock("./Home", () => ({ Home: () => <div>Home</div>}));
 
 describe("App", () => {
   it("renders successfully", () => {
-    const history = createMemoryHistory()
+    const history = createMemoryHistory();
     const { container } = render(
       <Router history={history}>
         <App />
       </Router>
-    )
-    expect(container.innerHTML).toMatch('Goblin Store')
-  })
-})
+    );
+    // https://jestjs.io/docs/expect
+    expect(container.innerHTML).toMatch('Goblin Store');
+  });
+
+  it("renders Home component on root route", () => {
+    const history = createMemoryHistory();
+    history.push("/");
+
+    /* The home component relies on ProductsProvider, so, we need to just
+    mock the component instead.
+     */
+    const { container } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    expect(container.innerHTML).toMatch("Home");
+  });
+});
