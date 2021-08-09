4503f4400a01f531c39f7c1c3ef6ac25df1244e9
Kyle Geib <kyle.geib@gmail.com>
2021-08-09 17:57:55 -0400

    Home testing in place!

--- testing/src/Home/Home.tsx
+++ testing/src/Home/Home.tsx
@@ -1,15 +1,37 @@
 import React from "react"
+import { Product } from "../shared/types"
 import { ProductCard } from "./ProductCard"
 import { useProducts } from "./useProducts"
-import { Category } from "../shared/types"
 
+/**
+ * The author of this lesson argues that by defining the different return values
+ * individually doesn't just make testing a bit easier, but actually makes
+ * future development a little cleaner because it requires the coder to more
+ * directly consider what values they're going to be using from the hook.
+ * Effectively they'll be required to define the new values individually as they
+ * add them.
+ */
+/*
+import { Category } from "../shared/types"
 interface HomeProps {
   useProductsHook?: () => {
     categories: Category[]
     isLoading: boolean
     error: boolean
   }
-}
+};
+*/
+
+/**
+ * But, alternatively, this is the sleeker way of going about defining this
+ * component's props.
+ */
+interface HomeProps {
+  useProductsHook?: () => Pick<
+    ReturnType<typeof useProducts>,
+    "categories" | "isLoading" | "error"
+  >,
+};
 
 export const Home = ({ useProductsHook = useProducts }: HomeProps) => {
   const { categories, isLoading, error } = useProductsHook()
@@ -29,7 +51,7 @@ export const Home = ({ useProductsHook = useProducts }: HomeProps) => {
           <section key={category.name} className="nes-container with-title showcase">
             <h2 className="title">{category.name}</h2>
             <section className="items">
-              {category.items.map((item) => {
+              {category.items.map((item: Product) => {
                 return <ProductCard key={item.name} datum={item} />
               })}
             </section>
--- testing/src/Home/index.ts
+++ testing/src/Home/index.ts
@@ -1,1 +1,10 @@
+/**
+ * Using index.ts files is a great way to combine closely related components
+ * into a single encompassing component to be used together. Smaller components
+ * and other utility modules then become slightly obfuscated from the normal
+ * import structure.
+ * That, and it also helps with organization work without needing to rely on
+ * long prefixes in your file names.
+ * Something interesting to consider in the future!
+ */
 export * from './Home'
