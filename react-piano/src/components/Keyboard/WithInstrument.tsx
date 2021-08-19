301eaeca83f7fc86d76d11f1aa3d98fe5998d87c
Kyle Geib <kyle.geib@gmail.com>
2021-08-19 18:30:10 -0400

    Support for different instruments!

--- react-piano/src/components/Keyboard/WithInstrument.tsx
+++ react-piano/src/components/Keyboard/WithInstrument.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
+import { useInstrument } from "../../state/Instrument";
 import { useAudioContext } from "../AudioContextProvider";
 import { useSoundFont } from "../../adapters/Soundfont/useSoundfont";
-import { useMount } from "../../utils/useMount";
 import { Keyboard } from "../Keyboard";
 
 // This is the code from the written guide. But it wouldn't work.
@@ -9,11 +10,19 @@ export const KeyboardWithInstrument = () => {
   // We're sure because we won't render this component unless we have audio
   // context -- that the browser supports it.
   const AudioContext = useAudioContext()!;
-  const { loading, play, stop, load } = useSoundFont({ AudioContext });
+  const { instrument } = useInstrument();
+  const { loading, current, play, stop, load } = useSoundFont({ AudioContext });
 
-  // Allows us to run some code right after a component is mounted into the DOM.
-  useMount(load);
-
+  /**
+   * "We replace useMount() hook with useEffect() hook. We have to do that since
+   * we want to dynamically change our instrument's sounds set, instead of
+   * loading it once when mounted."
+   */
+  useEffect(() => {
+    if (!loading && instrument !== current) {
+      load(instrument);
+    }
+  }, [load, loading, current, instrument]);
 
   return <Keyboard loading={loading} play={play} stop={stop} />;
 };
