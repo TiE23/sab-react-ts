8bd84581675667c6c92f47d79a5f41bc3b4aeacf
Kyle Geib <kyle.geib@gmail.com>
2022-04-27 22:02:51 -0400

    Getting trello-clone working again.

---
+++ trello-clone/src/backend/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response} from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = 4000;
+
+let lists: any[] = [];
+
+app.post("/save", (req: Request, res: Response) => {
+  console.log(req.body);
+  lists = req.body.lists;
+  return res.json({ success: true });
+})
+
+app.get("/load", (req: Request, res: Response) => res.json({ lists }));
+
+app.listen(port, () =>
+  console.log(`Kanban backend running on http://localhost:${port}!`)
+);
